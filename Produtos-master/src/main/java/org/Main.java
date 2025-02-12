99d646d0234f0640a952273e2cb57cf27866963b
MichaelDCA <michaeldouglascruzalves@gmail.com>
2025-02-11 23:47:12 -0300

    Update Main.java

--- Produtos-master/src/main/java/org/Main.java
+++ Produtos-master/src/main/java/org/Main.java
@@ -1,7 +1,12 @@
 package org;
+import org.View.IndexGUI;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Inicia a interface gráfica
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            IndexGUI gui = new IndexGUI(); // Cria a instância da interface gráfica
+            gui.setVisible(true); // Torna a janela visível
+        });
     }
 }
